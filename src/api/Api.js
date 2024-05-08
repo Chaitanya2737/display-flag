057613416ed034cf0c4eff0a78d2189ef6e55ccf
Chaitanya <chaitanyasatarkar123@gmail.com>
2024-05-08 13:21:44 +0530

    api error

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,6 +1,11 @@
 import axios from "axios"
 
 export const flag = async() => {
-    let res = await axios.get("https://restcountries.com/v3.1/all");
-    return res.data
+    try {
+        
+        let res = await axios.get("https://restcountries.com/v3.1/all");
+        return res.data
+    } catch (error) {
+        return null
+    }
 }
